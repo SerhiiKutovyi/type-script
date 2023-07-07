c51bbaaac85a41c94bc03f10127cb21c41c72f82
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-07-07 14:44:05 +0300

    new config

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,7 @@
-const fred = 'name'
+const fred = {
+  name: 'Max',
+  sename: 'Bur',
+  eyg: 10,
+  huhu: 23,
+};
+console.log('hbhh');
---
+++ src/button.ts
@@ -0,0 +1,5 @@
+const btn = document.querySelector('#buttonId')!;
+
+btn.addEventListener('click', () => {
+  console.log('Pool');
+});
