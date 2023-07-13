112ec9b0afb089fd708d2d8ad06699a55c8ed462
SerhiiKutovyi <109694632+SerhiiKutovyi@users.noreply.github.com>
2023-07-13 16:36:10 +0300

    Advanced Types

--- src/app.ts
+++ src/app.ts
@@ -124,3 +124,33 @@
 // showFields(admin);
 // showFields(empl);
 
+class Car {
+    drive() {
+        console.log('Driving...');
+        
+    }
+} 
+
+class Truck {
+    drive() {
+        console.log('Driving a truck...');
+    }
+    loadCargo(amount: number) {
+        console.log('Loading cargo ... ' + amount);
+        
+    }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+const useVehicle = (vehicle: Vehicle) => {
+    vehicle.drive();
+    if (vehicle instanceof Truck) {
+        vehicle.loadCargo(1000)
+    }
+};
+useVehicle(v1);
+useVehicle(v2);
